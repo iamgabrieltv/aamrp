04735429e484a57314a5528ceed6de15e272c8f9
Gabriel Sarcevic <gabrielsarcevic1@gmail.com>
2025-10-25 22:45:07 +0200

    forgot to commit before

--- AAMRP/Program.cs
+++ AAMRP/Program.cs
@@ -2,7 +2,10 @@
 using System.Net;
 using System.Net.Http.Headers;
 using System.Text.RegularExpressions;
+using System.Web;
 using DiscordRPC;
+using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
 using Newtonsoft.Json.Linq;
 using HtmlAgilityPack;
 
@@ -26,19 +29,51 @@ class Program
         public required string ArtistUrl;
         public required string CollectionUrl;
     }
+
+    private class SongData(string name, string artist, string album)
+    {
+        public string Name = name;
+        public string Artist = artist;
+        public string Album = album;
+    }
     
     static async Task<ArtworkUrls> FetchArtworkUrl(string song, string album, string artist)
     {
         Console.WriteLine(Environment.NewLine + $"Fetching Data for {song} - {artist} from {album}...");
 
-        var request = new HttpRequestMessage(HttpMethod.Get, $"https://itunes.apple.com/search?term={song} {album} {artist}&media=music&entity=song");
+        var uri = new UriBuilder("https://amp-api-edge.music.apple.com/v1/catalog/us/search");
+        var query = HttpUtility.ParseQueryString(uri.Query);
+        query["platform"] = "web";
+        query["l"] = "en-US";
+        query["limit"] = "1";
+        query["with"] = "serverBubbles";
+        query["types"] = "songs";
+        query["term"] = $"{song} {artist} {album}";
+        query["include[songs]"] = "artists";
+        uri.Query = query.ToString();
+
+        var request = new HttpRequestMessage(HttpMethod.Get, uri.Uri);
+        request.Headers.Accept.ParseAdd("*/*");
+        request.Headers.AcceptLanguage.ParseAdd("en-US,en;q=0.9");
+        request.Headers.Authorization = AuthenticationHeaderValue.Parse("eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldlYlBsYXlLaWQifQ.eyJpc3MiOiJBTVBXZWJQbGF5IiwiaWF0IjoxNzU5OTcwNjMxLCJleHAiOjE3NjcyMjgyMzEsInJvb3RfaHR0cHNfb3JpZ2luIjpbImFwcGxlLmNvbSJdfQ.2olNgPLuL51wQBjlYwZWslVBxqV65I921NlgdXHazA9DL_-zksa42Lr4aiGC0TV3SAe4vs9FSRtdKe9gTCCiwQ");
+        request.Headers.Add("Origin", "https://music.apple.com");
         
         // Make the request
         var response = await HttpClient.SendAsync(request);
         response.EnsureSuccessStatusCode();
         var jsonString = await response.Content.ReadAsStringAsync();
         var data = JObject.Parse(jsonString);
-        var songData = data["results"]?.First(s => s["collectionName"] != null && s["collectionName"]!.ToString().Equals(album, StringComparison.OrdinalIgnoreCase));
+        var songData = data["results"]![0];
+        try
+        {
+            songData = data["results"]?.First(s =>
+                s["collectionName"] != null &&
+                s["collectionName"]!.ToString().Equals(album, StringComparison.OrdinalIgnoreCase));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
         if (songData == null) throw new Exception("Song not found");
         Console.WriteLine("Done fetching");
         var coverUrl = songData["artworkUrl100"]!.ToString().Replace("100x100", "512x512");
@@ -110,7 +145,7 @@ class Program
             return $"https://aamrp.iamgabriel.dev/{filename}.avif";
         }
 
-        string filepath = $"/home/gabriel/Downloads/{filename}.avif";
+        string filepath = AppDomain.CurrentDomain.BaseDirectory + $"{filename}.avif";
         string arguments =
             $"-protocol_whitelist file,http,https,tcp,tls,crypto -i {url} -c:v libsvtav1 -preset 10 -crf 35 -r 30 -an -vf \"crop=min(iw\\,ih):min(iw\\,ih),scale=512:512\" \"{filepath}\"";
         ProcessStartInfo startInfo = new ProcessStartInfo
@@ -120,12 +155,12 @@ class Program
             RedirectStandardOutput = false,
             RedirectStandardError = false,
             UseShellExecute = true,
-            CreateNoWindow = false
+            CreateNoWindow = true
         };
 
         try
         {
-            Console.WriteLine("Starting Conversion with FFMPEG...");
+            Console.WriteLine($"Starting Conversion with FFMPEG... Writing to {filepath}");
             Process process = new Process { StartInfo = startInfo };
             process.Start();
             process.WaitForExit();
@@ -168,55 +203,168 @@ class Program
             return String.Empty;
         }
     }
-    
-    static void Main()
+
+    static SongData GetAppleMusicInfo()
     {
-        Console.WriteLine("Welcome to AAMRP!" + Environment.NewLine + "Animated Apple Music Rich Presence");
-
-        const string song = "Blue Moon";
-        const string album = "Midnight Sun";
-        const string artist = "Zara Larsson";
-
-        Setup();
-        var result = FetchArtworkUrl(song, album, artist).Result;
-        var artistUrl = FetchArtistArtworkUrl(result.ArtistUrl).Result;
-        RpcClient!.SetPresence(new RichPresence
+        var amProcesses = Process.GetProcessesByName("AppleMusic");
+        if (amProcesses.Length == 0)
         {
-            Type = ActivityType.Listening,
-            Details = song,
-            State = artist,
-            Assets = new Assets()
-            {
-                LargeImageKey = result.AlbumArt,
-                LargeImageText = album,
-                SmallImageKey = artistUrl,
-                SmallImageText = artist
-            }
-        });
+            Console.WriteLine("Couldn't find AppleMusic");
+            return null;
+        }
+        var windows = new List<AutomationElement>();
+        var automation = new UIA3Automation();
+        var processId = amProcesses[0].Id;
+        windows = [.. automation.GetDesktop().FindAllChildren(c => c.ByProcessId(processId))];
         
-        var animatedUrl = FetchAnimatedArtworkUrl(result.CollectionUrl).Result;
-        if (animatedUrl != String.Empty)
+                
+        if (windows.Count == 0) {
+            Console.WriteLine("No windows found on desktop, trying alternative search");
+            var vdesktopWin = FlaUI.Core.Application.Attach(processId).GetMainWindow(automation, TimeSpan.FromSeconds(3));
+            if (vdesktopWin != null) {
+                    windows.Add(vdesktopWin);
+            }
+        }
+        
+
+        AutomationElement? amSongPanel = null;
+        bool isMiniPlayer = false;
+        foreach (var window in windows)
         {
-            Console.WriteLine(animatedUrl);
-            var objectUrl = ConvertToAvif(animatedUrl, $"{album}-{artist}").Result;
-            if (objectUrl != String.Empty)
+            isMiniPlayer = window.Name == "Mini Player";
+            if (isMiniPlayer)
             {
-                Console.WriteLine($"Updating RPC to {objectUrl}");
-                RpcClient.SetPresence(new RichPresence
+                amSongPanel = window.FindFirstDescendant(cf => cf.ByClassName("InputSiteWindowClass"));
+                if (amSongPanel != null)
+                {
+                    break;
+                }
+            }
+            else
+            {
+                amSongPanel = window.FindFirstDescendant(cf => cf.ByAutomationId("TransportBar")) ?? amSongPanel;
+            }
+        }
+
+        if (amSongPanel == null)
+        {
+            Console.WriteLine("Couldn't find songpanel");
+            return null;
+        }
+
+        var songFieldsPanel = isMiniPlayer ? amSongPanel : amSongPanel.FindFirstChild("LCD");
+        var songFields = songFieldsPanel?.FindAllChildren(cf => cf.ByAutomationId("myScrollViewer")) ?? [];
+
+        if (!isMiniPlayer && songFields.Length != 2)
+        {
+            Console.WriteLine(("nothing playing"));
+            return null;
+        }
+
+        var songNameElement = songFields[0];
+        var songAlbumArtistElement = songFields[1];
+
+        if (songNameElement.BoundingRectangle.Bottom > songAlbumArtistElement.BoundingRectangle.Bottom)
+        {
+            songNameElement = songFields[1];
+            songAlbumArtistElement = songFields[0];
+        }
+
+        var songName = songNameElement.Name;
+        var songAlbumArtist = songAlbumArtistElement.Name;
+
+        string songArtist = "";
+        string songAlbum = "";
+        var songSplit = songAlbumArtist.Split(" \u2014 ");
+        if (songSplit.Length > 1)
+        {
+            songArtist = songSplit[0];
+            songAlbum = songSplit[1];
+        }
+        else
+        {
+            songArtist = songSplit[0];
+            songAlbum = songSplit[0];
+        }
+        return new SongData(songName, songArtist, songAlbum);
+    }
+
+    static async Task Start()
+    {
+       var currentSong = new SongData(null, null, null);
+        while (true)
+        {
+            var newSong = GetAppleMusicInfo();
+
+            if (newSong.Name == currentSong.Name)
+            {
+                Console.WriteLine($"Status unchanged. Current: {currentSong.Name}");
+            }
+            else
+            {
+                currentSong = newSong;
+                var song = currentSong.Name;
+                var album = currentSong.Album;
+                var artist = currentSong.Artist;
+                
+                var result = await FetchArtworkUrl(song, album, artist);
+                var artistUrl = await FetchArtistArtworkUrl(result.ArtistUrl);
+                RpcClient!.SetPresence(new RichPresence
                 {
                     Type = ActivityType.Listening,
                     Details = song,
                     State = artist,
                     Assets = new Assets()
                     {
-                        LargeImageKey = objectUrl,
+                        LargeImageKey = result.AlbumArt,
                         LargeImageText = album,
                         SmallImageKey = artistUrl,
                         SmallImageText = artist
                     }
                 });
+        
+                var animatedUrl = await FetchAnimatedArtworkUrl(result.CollectionUrl);
+                if (animatedUrl != String.Empty)
+                {
+                    Console.WriteLine(animatedUrl);
+                    var objectUrl = await ConvertToAvif(animatedUrl, $"{album}-{artist}");
+                    if (objectUrl != String.Empty)
+                    {
+                        Console.WriteLine($"Updating RPC to {objectUrl}");
+                        RpcClient.SetPresence(new RichPresence
+                        {
+                            Type = ActivityType.Listening,
+                            Details = song,
+                            State = artist,
+                            Assets = new Assets()
+                            {
+                                LargeImageKey = objectUrl,
+                                LargeImageText = album,
+                                SmallImageKey = artistUrl,
+                                SmallImageText = artist
+                            }
+                        });
+                    }
+                }
             }
+
+            await Task.Delay(5000);
         }
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine("Welcome to AAMRP!" + Environment.NewLine + "Animated Apple Music Rich Presence");
+        Setup();
+        Thread staThread = new Thread(() =>
+        {
+            Start().Wait();
+        });
+        staThread.SetApartmentState(ApartmentState.STA);
+        staThread.IsBackground = true; 
+        staThread.Start();
+        
+        Console.WriteLine("Background polling thread started. Press any key to exit.");
         
         // Cleanup
         Console.ReadKey();
